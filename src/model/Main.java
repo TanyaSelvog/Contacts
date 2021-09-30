eda94d6a4dc6655f333b120def2a9f1a4532add8
Tanya Selvog <tanyaselvog@gmail.com>
2021-09-29 20:42:12 -0700

    Initial commit

---
+++ src/model/Main.java
@@ -0,0 +1,23 @@
+package model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/mainWindowPane.fxml"));
+        primaryStage.setTitle("Contacts");
+        primaryStage.setScene(new Scene(root, 687, 452));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
